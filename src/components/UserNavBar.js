7ed068c67a18231699ee709d209a633c6a1825aa
amlane <twelvexstring@gmail.com>
2020-01-07 14:32:48 -0600

    fixed nav bar width

--- src/components/UserNavBar.js
+++ src/components/UserNavBar.js
@@ -13,7 +13,8 @@ const UserNavBar = ({ logOut }) => {
         justifyContent: "space-between",
         alignItems: "center",
         flexWrap: "wrap",
-        margin: "0 auto"
+        margin: "0 auto",
+        maxWidth: "1600px"
       }}
     >
       <NavLink to="/" style={{ textDecoration: "none" }}>
